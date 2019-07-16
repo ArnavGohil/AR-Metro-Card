26cae2f55c3e9c0d73bebb34fe771f6f8930094c
Arnav <arnav.gohil04@gmail.com>
2019-07-16 12:39:23 +0530

    Commit

--- ARCARD/Assets/ButtonGreen.cs
+++ ARCARD/Assets/ButtonGreen.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class ButtonGreen : MonoBehaviour
+public class ButtonGreen : MonoBehaviour , IVirtualButtonEventHandler
 {
+
+    public GameObject green;
+
+    public Animator cubeAni;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+     green = GameObject.Find("Green_Line"); 
+     green.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+     cubeAni.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
-        
+        cubeAni.Play("anim");
+        Debug.Log("Button is Clicked");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button is Released");
+
+
     }
 }
